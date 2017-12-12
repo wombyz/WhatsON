487c34222037c70a8da9a081cb9c7c77df352809
Liam Ottley <liamottley@dykemodge.local>
2017-12-13 08:40:39 +1300

    Initial Commit

---
+++ WhatsON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatsON
+//
+//  Created by Liam Ottley on 13/12/17.
+//  Copyright © 2017 Liam Ottley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
